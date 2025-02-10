86fc731efd7c5f1e79c027163922aaff36689074
damir <toriya.damir.2004@gmail.com>
2025-02-10 19:38:21 +0500

    [FT-23][TASK] comment fix

--- frontend/src/types/task.ts
+++ frontend/src/types/task.ts
@@ -1,11 +1,11 @@
 export interface Task {
-    id: number;
-    title: string;
-    description: string | null;
-    priority: "low" | "medium" | "high";
-    category: "work" | "study" | "personal";
-    due_date: string | null;
-    is_completed: boolean; // ✅ Теперь используем только is_completed
+    id:           number;
+    title:        string;
+    description:  string | null;
+    priority:     "low" | "medium" | "high";
+    category:     "work" | "study" | "personal";
+    due_date:     string | null;
+    is_completed: boolean;
 }
 
 export type CreateTask = Omit<Task, "id">;
